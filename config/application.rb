2f2a16adf7361f12f3ecc1482cb4bed874b47ec1
kohama66 <54892265+kohama66@users.noreply.github.com>
2022-01-01 23:46:41 +0900

    add cors

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,14 @@ module AuctionApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "http://localhost:8080"
+        resource "*",
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete]
+      end
+    end
   end
 end
